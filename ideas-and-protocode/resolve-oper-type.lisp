2a82c74a505ed0c0105e427e5998fd05642d1419
Chris Bagley <chris.bagley@gmail.com>
2013-01-03 21:37:18 +0000

    WIP: I'm changing how blocks work. Really it now an inappropriate name Also operators will now be functions with macros to prettify them.

--- ideas-and-protocode/resolve-oper-type.lisp
+++ ideas-and-protocode/resolve-oper-type.lisp
@@ -65,3 +65,120 @@
 ;;       if first is vec then second must be vec or scalar
 ;; & | ^ int or uint or ivec* of same size      
 ;; 
+
+;; treat % , << >> & | ^ && || ^^ as function
+;; maybe most of these can be treated as macros which result in functions
+;; e.g. (|| 1 2 3) can result in (%|| 1 (%|| 2 3))
+;;   or (+ 2 3 4) can result in (%+ 2 (%+ 3 4)
+;; then the operators can be functions which only need to be concerned with
+;; left and right operands. The result will be syntactically correct
+
+;; sequence operator does not like literals...don't know why see 4.20 glsl spec..WRONG!!!
+;;  it is totally cool to do this but it cannot be used as a CONSTANT expression 
+;;  (read up on them), basically it means we couldnt define a glsl array with (2,3) as the
+;;  length.
+
+;; ok so now the final one...do we force + - * / to take a max of two args?
+;;      pros: easy to implement/control
+;;            removes another layer of complexity from compiler
+;;       
+;; negatives: unnecesary parens (hehe worrying about parens in lisp hehe)
+
+;; '(making (making games) a game)
+
+;; fiddly fecker test
+(let ((a 1)
+      (b 2.4))
+  (let (c (* a b))
+    (setf b 3.0)
+    (setf c (+ b 1.0))
+    (% c 2))
+  (jam 1 2))
+
+;; int a = 1;
+;; float b = 2.4;
+;; float c = a * b;
+;; (((b = 3.0),
+;;   (c = (b + 1.0)),
+;;   (c % 2)),
+;;  jam(1, 2));
+
+;; what if we don't allow progn?
+;; seems drastic we should test this first
+
+(jam (let ((a 4))
+       (setf a (* a 2))
+       (let ((b a))
+         (thing b))))
+;; int a = 4;
+;; a = (a * 2);
+;; int b = a;
+;; jam(thing(b));
+
+
+(jam (let ((a 4))
+       (setf a (* a 2))
+       (setf c (let ((b a)) (thing b)))))
+;; so for function compiling, if code has multiple lines then pick last one
+;; let type based on last expression
+;; need %block for this.
+
+(jam (let ((a 1)) 
+       (man (setf a (+ a 1)) 2)))
+
+;; int a = 1;
+;; a = a + 1;
+;; jam(man(a 2));
+
+;; add this to utils - its from ansi common lisp
+(defun compose (&rest fns)
+  (destructuring-bind (fn1 . rest) (reverse fns)
+    #'(lambda (&rest args)
+        (reduce #'(lambda (v f) (funcall f v))
+                rest
+                :initial-value (apply fn1 args)))))
+
+
+;; block logic
+;; if only one arg then just passthrough
+;; else make a list of each args block code follwed by line-code
+;; but only include block of last arg
+;; '(arg-1-block arg-1-code arg-2-block arg-2-code.. arg-n-block)
+;;  ^^ this is new block
+;; arg-n-code is now the current line and type.
+;; This covers progn but others are more tricky
+
+;;this is ok
+(let ((meh 1))
+  1
+  2
+  3)
+
+(progn (%typeify (setf meh 1))
+       1
+       2
+       3)
+
+;;let obviously replaces tokens where needed
+(let ((a 4.2))
+  (toast (let ((a 2)) (jam a))
+         a))
+
+(progn (%typeify (setf a 4.2))
+       (toast (progn (%typeify (setf _a 2))
+                     (jam _a))
+              a))
+;; that'll work too
+
+(man (setf a (+ a 1)) 2)
+;; if we are strict then the a=a+1 should be block and line should 
+;; be man (a,2).... not too urgent though
+
+
+;; [TODO] make the %typify function. type is void only adds type to line code.
+;; [TODO] add strict mode where setf's type if void
+;; [TODO] rename code and to-block to current-line and ?cache?.hmmm
+;; [TODO] make progn special
+;; [TODO] remove all parts of function spec which deal with block...its no
+;;        longer relevant
+
--- umbra.lisp
+++ umbra.lisp
@@ -22,9 +22,8 @@
     ((null cglsl-code) nil)
     ((typep cglsl-code 'code) cglsl-code)
     ((atom cglsl-code) (error "'~s' is unidentified." cglsl-code))
-    ((glsl-operatorp (first cglsl-code))
-     (glsl-compile-operator 
-      (first cglsl-code) (mapcar #'cglsl->glsl (rest cglsl-code))))
+    ((special-funcp (first cglsl-code)) 
+     (call-special (first cglsl-code)))
     ((glsl-function (first cglsl-code))
      (glsl-compile-function
       (first cglsl-code) (mapcar #'cglsl->glsl (rest cglsl-code))))
@@ -32,45 +31,38 @@
 
 ;;------------------------------------------------------------
 
-(defun glsl-compile-operator (op-name arg-objs)
-  (let* ((arg-code (mapcar #'code arg-objs))
-	 (code 
-	   (cond ((> (length arg-objs) 1)
-		  (format nil 
-			  (format nil "(~~{~~s ~~^~s ~~})" op-name)
-			  arg-code))
-		 ((eq 1 (length arg-objs)) 
-		  (if (glsl-unary-operator op-name)
-		      (format nil "(~s ~s)" op-name (car arg-code))
-		      (error "The GLSL operator '~s' is not an unary operator. It must take at least 2 arguments" op-name)))
-		 (t (error "Invalid number of arguments ~s for operator ~s" (length arg-objs) op-name)))))
-    (make-instance 'code 
-		   :type (glsl-resolve-oper-type op-name arg-objs)
-		   :code code
-		   :to-block (mapcan #'code-to-block arg-objs)
-		   :to-top (mapcan #'code-to-top arg-objs))))
-
-;;------------------------------------------------------------
-
 ;; expects code objects 
 (defun glsl-compile-function (func-name arg-objs)
   (let ((func-specs (gethash func-name *glsl-functions*)))
     (loop :for f-spec :in func-specs 
-	  :if (glsl-valid-function-args f-spec arg-objs )
-	    :return (make-instance 
-		     'code 
-		     :type (glsl-resolve-func-type f-spec arg-objs)
-		     :code (when (not (func-block f-spec))
-			     (apply 
-			      #'format 
-			      (append (list nil (func-body f-spec))
-				      (mapcar #'code arg-objs))))
-		     :to-block (append (mapcan #'code-to-block
-					       arg-objs)
-				       (when (func-block f-spec)
-					 (mapcar #'code arg-objs)))
-		     :to-top (mapcan #'code-to-top arg-objs))
-	  :finally (error "There is no applicable method for the ~%glsl function '~s'~%when called with argument types: ~s " func-name (mapcar #'code-type arg-objs)))))
+       :if (glsl-valid-function-args f-spec arg-objs )
+       :return (make-instance 
+                'code 
+                :type (glsl-resolve-func-type f-spec arg-objs)
+                :code (apply #'format 
+                             (append (list nil (func-body f-spec))
+                                     (mapcar #'code arg-objs)))
+                :to-block  (mapcan #'code-to-block arg-objs)
+                :to-top (mapcan #'code-to-top arg-objs))
+       :finally (error "There is no applicable method for the ~%glsl function '~s'~%when called with argument types: ~s " func-name (mapcar #'code-type arg-objs)))))
+
+(defun %progn (arg-objs)
+  (if (eq 1 (length arg-objs))
+      (car arg-objs)
+      (let ((last-arg (car (last arg-objs)))
+	    (args (subseq arg-objs 0 (- (length arg-objs) 1))))
+	(make-instance 
+	 'code 
+	 :type (glsl-resolve-func-type f-spec arg-objs)
+	 :code (code last-arg)
+	 :to-block (format nil "~{~%~s~%~s~}" 
+			   (append
+			    (mapcan #'(lambda (x) 
+					 (list (code-to-block x)
+					       (code x))) 
+				    args)
+			    (list (code-to-block last-arg))))
+	 :to-top (mapcan #'code-to-top arg-objs)))))
 
 ;;------------------------------------------------------------
 
