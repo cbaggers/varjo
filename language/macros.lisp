f6f5b1e0ad360c9760c8b2788579a78fce9d7264
Baggers <chris.bagley@gmail.com>
2015-07-05 01:20:59 +0200

    'and' is a special form now, not a macro, remove old macro definition

--- language/macros.lisp
+++ language/macros.lisp
@@ -8,15 +8,6 @@
 
 (in-package :varjo)
 
-(v-defmacro :and (&rest forms)
-  (let ((len (length forms)))
-    (assert (> len 0))
-    (if (= len 1)
-        (first forms)
-        `(if ,(first forms)
-             (and ,@(rest forms))
-             nil))))
-
 (v-define-compiler-macro :+ (&rest numbers)
   (cond
     ((= (length numbers) 1) (first numbers))
