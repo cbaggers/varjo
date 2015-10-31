fdce3c747188ea4806fcafc06fd897b5529b2b7b
Baggers <baggers@sett.lan>
2015-10-31 02:44:07 +0100

    Catch more errors in vtype-existsp
    make-instance was being called with any old symbol, of course this may
    throw an error. By catching this and returning nil we let other parts of
    the code handle the type issue gracefully.

--- compiler/environment.lisp
+++ compiler/environment.lisp
@@ -320,9 +320,12 @@
 
 ;;[TODO] really no better way of doing this?
 (defun vtype-existsp (type-name)
-  (and type-name (handler-case (progn (typep t type-name) t) (error () nil))
-       (or (typep (make-instance type-name) 'v-t-type)
-           (typep (make-instance type-name) 'v-spec-type))))
+  (and type-name
+       (handler-case (progn (typep t type-name) t) (error () nil))
+       (handler-case (progn (or (typep (make-instance type-name) 'v-t-type)
+				(typep (make-instance type-name) 'v-spec-type))
+			    t)
+	 (error () nil))))
 
 ;;-------------------------------------------------------------------------
 
