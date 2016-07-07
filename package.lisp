a420d583913c5690306814c3dd0feacc9c538968
Baggers <chris.bagley@gmail.com>
2016-07-07 20:13:54 +0200

    begin adding the various vector constructors

--- package.lisp
+++ package.lisp
@@ -4,7 +4,10 @@
   (:use #:cl #:rtg-math.base-maths)
   (:import-from :rtg-math :v! :m! :s~ :radians :degrees)
   (:import-from :rtg-math.base-vectors :x :y :z :w
-		:v!int :v!uint :v!bool :v!double)
+		:v!int :v!uint :v!bool :v!double
+		:v2! :v2!double :v2!int :v2!uint
+		:v3! :v3!double :v3!int :v3!uint
+		:v4! :v4!double :v4!int :v4!uint)
   (:import-from :rtg-math.vectors
 		:swizzle
 		:dot
@@ -135,7 +138,11 @@
 	#:named-readtables #:varjo-conditions)
   (:import-from :rtg-math :v! :m! :s~ :radians :degrees)
   (:import-from :rtg-math.base-vectors :x :y :z :w
-		:v!int :v!uint :v!bool :v!double)
+		:v!int :v!uint :v!bool :v!double
+		:v!int :v!uint :v!bool :v!double
+		:v2! :v2!double :v2!int :v2!uint
+		:v3! :v3!double :v3!int :v3!uint
+		:v4! :v4!double :v4!int :v4!uint)
   (:import-from :rtg-math.vectors
 		:swizzle
 		:dot
