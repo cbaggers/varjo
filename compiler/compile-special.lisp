c5aaeeaafbc68e365b3247e13754f0934db2a9be
Baggers <chris.bagley@gmail.com>
2017-01-08 16:18:29 +0100

    don't catch&rethrow errors in compile-special-func
    With this change we get much better stack traces.
    
    This should have been done ages ago but every time I looked at it I was
    in the middle of something else

--- compiler/compile-special.lisp
+++ compiler/compile-special.lisp
@@ -3,22 +3,9 @@
 
 ;;----------------------------------------------------------------------
 
-;;[TODO] Maybe the error should be caught and returned,
-;;       in case this is a bad walk
-;;{TODO} expand on this please. 'Future-you' couldnt work out what this meant
-;; {TODO} you from both of your futures here. I think he was saying that
-;;        the errors coming out of a special function could have been the result
-;;        of the special func using #'compile-form which tried compiling a
-;;        function call but while testing for the right function it threw and
-;;        error. I think that is wrong as the handler-case in compiler/functions
-;;        should catch those. We need to review all this stuff anyway.
-;;        In the case of special funcs there should never be any ambiguity, it
-;;        HAS to be the correct impl
 (defun compile-special-function (func args env)
   (multiple-value-bind (code-obj new-env)
-      (handler-case (apply (first (v-return-spec func)) (cons env args))
-        (varjo-error (e) (invoke-debugger e)))
-    ;;(assert (node-tree code-obj))
+      (apply (first (v-return-spec func)) (cons env args))
     (values code-obj new-env)))
 
 ;;----------------------------------------------------------------------
