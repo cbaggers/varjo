78861232312afecfa26cfd2cd61735964b64f641
Baggers <chris.bagley@gmail.com>
2017-01-23 17:32:51 +0100

    export test names from package

--- tests/package.lisp
+++ tests/package.lisp
@@ -1,3 +1,12 @@
 (uiop:define-package #:varjo.tests
     (:use #:cl #:varjo #:varjo-lang #:rtg-math #:rtg-math.base-maths #:fiveam)
-  (:export))
+  (:export :test-all
+           :void-tests
+           :build-tests
+           :struct-tests
+           :stemcell-tests
+           :flow-control-tests
+           :symbol-macro-tests
+           :first-class-func-tests
+           :external-functions-tests
+           :multiple-value-return-tests))
