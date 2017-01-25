3b4bda7b052ede565d3c94837ad1414fe5faca27
Baggers <chris.bagley@gmail.com>
2017-01-26 00:47:23 +0100

    remove some old redundant code

--- compiler/code-object.lisp
+++ compiler/code-object.lisp
@@ -61,12 +61,6 @@
          :place-tree (listify place-tree)
          :node-tree node-tree))
 
-(defun make-none-ob ()
-  (let ((none-type (gen-none-type)))
-    (make-code-obj
-     none-type nil
-     :node-tree (ast-node! :none nil none-type nil nil))))
-
 ;;- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 (defmethod copy-code ((code-obj code)
