448ace5e9e115daf1b6fd3f92ab7d6576b7112b6
Baggers <chris.bagley@gmail.com>
2017-04-17 01:55:48 +0200

    Move 'the' into declarations.lisp
    'The' is part of the declaration system:
    
    https://www.cs.cmu.edu/Groups/AI/html/cltl/clm/node103.html

--- language/special-operators/declarations.lisp
+++ language/special-operators/declarations.lisp
@@ -17,3 +17,24 @@
   (vbind (body declarations) (extract-declares body)
     (compile-declares declarations env)
     (compile-form `(progn ,@body) env)))
+
+;;------------------------------------------------------------
+;; The
+
+(v-defspecial the (type-name form)
+  :args-valid t
+  :return
+  (let* ((compiled (compile-form form env))
+         (obj (if (stemcellp (code-type compiled))
+                  (add-type-to-stemcell-code compiled type-name)
+                  (if (v-typep (code-type compiled)
+                               (type-spec->type type-name))
+                      compiled ;{TODO} proper error here
+                      (error "Incorrect declaration that ~a was of type ~a"
+                             compiled type-name)))))
+    (values
+     (copy-code
+      obj
+      :node-tree (ast-node! 'the (list type-name (node-tree compiled))
+                            (code-type compiled) env env))
+     env)))
