c0962728685cb567b0d00c0447b173abafbc1c1c
cbaggers <chris.bagley@gmail.com>
2017-11-25 20:12:45 +0100

    add test that shows bug in compute shader ast

--- tests/assignment-tests.lisp
+++ tests/assignment-tests.lisp
@@ -1,6 +1,13 @@
 (in-package :varjo.tests)
 (5am:in-suite assignment-tests)
 
+;;-----------------
+
+(v-defstruct bah ()
+  (data (:int 100)))
+
+;;-----------------
+
 (5am:def-test assign-0 (:suite assignment-tests)
   (finishes-p
    (compile-vert ((a :int)) :450 nil
@@ -23,6 +30,23 @@
      (let ((x 10))
        (incf x 1)
        (v! 0 0 0 0)))))
+
+(5am:def-test assign-3 (:suite assignment-tests)
+  (finishes-p
+   (compile-compute (&uniform (woop bah :ssbo)) :450 nil
+     (declare (local-size :x 1 :y 1 :z 1))
+     (setf (aref (bah-data woop) (int (x gl-work-group-id)))
+           999)
+     (values))))
+
+(5am:def-test assign-4 (:suite assignment-tests)
+  (signals varjo-conditions:assigning-to-readonly
+    (compile-compute (&uniform (woop bah :ssbo)) :450 nil
+      (declare (local-size :x 1 :y 1 :z 1))
+      (setf (aref (bah-data woop) (int (x gl-work-group-id)))
+            999)
+      (setf woop woop)
+      (values))))
 
 ;; (5am:def-test assign-3 (:suite assignment-tests)
 ;;   (finishes-p
