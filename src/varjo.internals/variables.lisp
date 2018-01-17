0d7e068d05033c57b8440b0605cbd6fa609319d1
Baggers <chris.bagley@gmail.com>
2018-01-17 16:38:08 +0100

    dont add glsl vars to env if not supported in this version

--- src/varjo.internals/variables.lisp
+++ src/varjo.internals/variables.lisp
@@ -28,20 +28,24 @@
   (equal (glsl-name a) (glsl-name b)))
 
 (defun add-glsl-vars (env)
-  (labels ((add-vars (vars)
-             (loop :for (lisp-name glsl-name type-spec setable) :in vars :do
-                (let ((type (type-spec->type type-spec (%gl-flow-id!))))
-                  (%add-symbol-binding
-                   lisp-name (v-make-value
-                              type env :glsl-name glsl-name
-                              :read-only (not setable))
-                   env)
-                  (add-reserved-lisp-name lisp-name env glsl-name)))))
-    (add-vars (assocr t *glsl-variables*))
-    (loop :for kind :in *stage-type-names*
-       :when (typep (stage env) kind)
-       :do (add-vars (assocr kind *glsl-variables*)))
-    env))
+  (let ((version (get-version-from-context env)))
+    (labels ((add-vars (vars)
+               (loop :for (lisp-name glsl-name type-spec setable versions)
+                  :in vars :do
+                  (when (find version versions)
+                    (let ((type (type-spec->type type-spec (%gl-flow-id!))))
+                      (%add-symbol-binding
+                       lisp-name
+                       (v-make-value type env
+                                     :glsl-name glsl-name
+                                     :read-only (not setable))
+                       env)
+                      (add-reserved-lisp-name lisp-name env glsl-name))))))
+      (add-vars (assocr t *glsl-variables*))
+      (loop :for kind :in *stage-type-names*
+         :when (typep (stage env) kind)
+         :do (add-vars (assocr kind *glsl-variables*)))
+      env)))
 
 ;;--------------------------------------------------
 
