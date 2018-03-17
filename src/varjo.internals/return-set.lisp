b61ff610a934ac60200fc7a772862f1509e559eb
baggers <baggers@cave>
2018-03-17 22:53:51 +0100

    optimize merge-return-sets a little
    Thsi showed up kinda high in the perf logs so knocked a little time
    off it. It's still not where it could be though

--- src/varjo.internals/return-set.lisp
+++ src/varjo.internals/return-set.lisp
@@ -1,20 +1,26 @@
 (in-package :varjo.internals)
 (in-readtable :fn.reader)
 
-(defun qualified-eql (ret-a ret-b)
-  (and (v-type-eq ret-a ret-b)
-       (= (length (qualifiers ret-a)) (length (qualifiers ret-b)))
-       (every #'qualifier= (qualifiers ret-a) (qualifiers ret-b))))
-
-(defun type-sets-equal (set-a set-b)
-  (and (= (length set-a) (length set-b))
-       (every #'qualified-eql set-a set-b)))
-
 (defun merge-return-sets (sets)
-  (labels ((%merge-return-sets (set-a set-b)
+  (declare (optimize (speed 3)))
+  (labels ((qualified-eql (ret-a ret-b)
+             (declare (type v-type ret-a ret-b))
+             (and (v-type-eq ret-a ret-b)
+                  (let ((qa (qualifiers ret-a))
+                        (qb (qualifiers ret-b)))
+                    (declare (type list qa qb))
+                    (and (= (length qa) (length qb))
+                         (every #'qualifier= qa qb)))))
+
+           (type-sets-equal (set-a set-b)
+             (declare (type vector set-a set-b))
+             (and (= (length set-a) (length set-b))
+                  (every #'qualified-eql set-a set-b)))
+
+           (%merge-return-sets (set-a set-b)
              (assert (type-sets-equal set-a set-b)
                      () 'return-type-mismatch
-                     :sets (list set-a set-b))
+                     :sets sets)
              set-a))
     (let* ((sets (remove nil sets)))
       (reduce #'%merge-return-sets (rest sets)
