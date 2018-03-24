0741c0808d080a04e51bb94b5c194c58e5fdf76f
cbaggers <chris.bagley@gmail.com>
2018-03-24 01:28:16 +0100

    Better error message for missing clauses in cond

--- src/vari.cl/macros.lisp
+++ src/vari.cl/macros.lisp
@@ -5,8 +5,10 @@
      #'lmbda))
 
 (v-defmacro cond (&rest clauses)
+  (assert clauses ()
+          "Varjo: COND must contain at least 1 clause")
   `(if ,(caar clauses)
        (progn
-         ,@(cdar clauses))
+         ,@(or (cdar clauses) '((values))))
        ,@(when (rest clauses)
            `((cond ,@(rest clauses))))))
