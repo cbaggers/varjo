20cb51c6be973c2cf4104cd1fae50cb7d7420fc7
baggers <baggers@cave>
2018-05-27 14:56:16 +0200

    let was not remove multi-env-base when compiling bindings
    This resulted in assigning to a non-existant out-var when multi-return
    function was called from the main function

--- src/vari.cl/special-operators/let.lisp
+++ src/vari.cl/special-operators/let.lisp
@@ -8,12 +8,13 @@
   :args-valid t
   :return
   (let* ((binding-names (mapcar λ(nth-or-self 0 _) bindings))
-         (dup-names (find-duplicates binding-names)))
+         (dup-names (find-duplicates binding-names))
+         (base (v-multi-val-base env)))
     (assert (not dup-names) () 'dup-names-in-let :names dup-names)
     (unless body (error 'body-block-empty :form-name 'let))
     (vbind (body declarations) (extract-declares-and-doc-string body)
       (vbind ((new-var-objs nil body-obj) final-env)
-          (with-fresh-env-scope (fresh-env env)
+          (with-fresh-env-scope (fresh-env env :multi-val-base nil)
             (env-> (p-env fresh-env)
               (compile-forms-not-propagating-env-returning-list-of-compiled
                (lambda (p-env binding)
@@ -21,7 +22,8 @@
                    (compile-let name type-spec value-form p-env nil nil)))
                p-env bindings)
               (compile-declares declarations p-env)
-              (compile-form `(progn ,@body) p-env)))
+              (with-fresh-env-scope (based-env p-env :multi-val-base base)
+                (compile-form `(progn ,@body) based-env))))
         (let* ((merged (merge-progn (list (merge-multi-env-progn new-var-objs)
                                           body-obj)
                                     env final-env)))
